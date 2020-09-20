b201f98fb2945cf9f91f003cd7a48b88082283e0
Raymond Thompson <68765572+ray1842@users.noreply.github.com>
2020-09-20 16:41:48 -0400

    Updates

---
+++ precache-manifest.48507ee496f3cf3ef34950892e56694b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa7076cab8760259048f2a778ad3a389",
+    "url": "/Basic-Calculator/index.html"
+  },
+  {
+    "revision": "65cd5452d8dd3d82ff5c",
+    "url": "/Basic-Calculator/static/css/main.d6d44a2b.chunk.css"
+  },
+  {
+    "revision": "b222860d17a9ea5f41ac",
+    "url": "/Basic-Calculator/static/js/2.8f2b7201.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/Basic-Calculator/static/js/2.8f2b7201.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65cd5452d8dd3d82ff5c",
+    "url": "/Basic-Calculator/static/js/main.01ec9cd2.chunk.js"
+  },
+  {
+    "revision": "bb731211443fee31c804",
+    "url": "/Basic-Calculator/static/js/runtime-main.f1160e66.js"
+  }
+]);
